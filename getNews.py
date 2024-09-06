b32e707a8291426c906acfe45b315094c2b1ab8b
Prithvee Raj Singh <132699827+pri-cd@users.noreply.github.com>
2024-09-06 07:12:04 +0000

    Added GNews API

--- getNews.py
+++ getNews.py
@@ -1,7 +1,42 @@
 from time import sleep
+from datetime import datetime, timezone, timedelta
 import requests
 import os
 
+gNewsUrl = "https://gnews.io/api/v4/top-headlines"
+
+
+def getTime():
+    now_utc = datetime.now(timezone.utc)
+    formatted_time = now_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+    print("Formatted UTC Time:", formatted_time)
+    return formatted_time
+
 
 def get_business_news():
-    pass
+    apiKey = os.getenv('NEWS_API_KEY')
+    params = {
+        'category': 'business',
+        'lang': 'en',
+        'country': 'in',
+        'max': 1,
+        'apikey': apiKey
+    }
+
+    try:
+        resp = requests.request(method='GET', url=gNewsUrl, params=params)
+    except Exception as exe:
+        print(
+            f"Exception Occured While Fetching News Using- GNEWS API!- {exe}")
+        return None, None, None
+
+    articles = resp.json().get('articles', [])
+    title, desc, url = None, None, None
+
+    for article in articles:
+        title = article.get('title', 'No Title')
+        desc = article.get('description', 'No Description')
+        url = article.get('url', 'No URL')
+
+    print(title, desc, url, sep="\n")
+    return title, desc, url
