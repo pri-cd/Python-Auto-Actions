372a251828c205a3de9c9e3cfcde77d5024aacd1
Prithvee Raj Singh <132699827+pri-cd@users.noreply.github.com>
2024-09-09 05:15:11 +0000

    Added ChatID For Me

--- sendMsg.py
+++ sendMsg.py
@@ -3,7 +3,7 @@ import time
 import os
 
 
-def get_chat_ids(token):
+def get_chat_ids_from_api(token):
     url = f'https://api.telegram.org/bot{token}/getUpdates'
     response = requests.get(url)
     updates = response.json()
@@ -17,32 +17,39 @@ def get_chat_ids(token):
                 chat_ids.append(chat_id)
         return chat_ids
     else:
-        raise Exception("No Chat-IDs Found!-")
+        print("No Chat-IDs Found!-")
+        return chat_ids
 
 
 '''
-    @brief: Send The Message To All The Chat-Id Who're Accessing My ChatBot!-
+    @brief: Send The Message To Only My Chat Id!
     @credits: Telegram Chat Bot APIs
 '''
 
 
 def send_telegram_message(body):
     bot_token = os.getenv('TG_CHAT_TOKEN')
-    chat_ids = get_chat_ids(bot_token)
-
-    if not chat_ids:
-        raise Exception("No chat IDs available to send the message.")
-
+    chat_ids = get_chat_ids_from_api(bot_token)
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
-    response = None  # Initialize the response variable
+
+    if len(chat_ids) < 1:
+        print("No chat IDs available to send the message.")
+        print("Sending To Only Pri!")
+
+    chat_ids.append(os.getenv("TG_CHAT_ID"))
+    print(f"Chat IDs- ALL: {chat_ids}")
+    response = None
+
+    # Removing Duplicates!-
+    chat_ids = list(set(chat_ids))
+    print(f"Chat IDs- FINAL!!!: {chat_ids}")
 
     print(" ============= Starting Send!=============== ")
     for chat_id in chat_ids:
         print(f"Sending message to chat ID: {chat_id}")
         payload = {'chat_id': chat_id, 'text': body}
         response = requests.post(url, data=payload)
+        time.sleep(1)
     print(" ============= Ending Send!=============== ")
 
-    # To Not overload The Telegram API(s).
-    time.sleep(1)
     return response.status_code if response else None
